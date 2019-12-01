f86a434c5f55515f5fab07d51fc316a217205ba3
mrityunjay <mrityunjay.karn@paytm.com>
2019-12-01 21:22:49 +0530

    first commit

---
+++ build/precache-manifest.7718ba9f7c5e873db437830a28e45f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391a716f83cabeb09f2e30e455bb8317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dae5954b9e50f1fda1c9",
+    "url": "/static/css/main.748c88c4.chunk.css"
+  },
+  {
+    "revision": "efbd4f9b997e0ee1e5a8",
+    "url": "/static/js/2.88cbedeb.chunk.js"
+  },
+  {
+    "revision": "dae5954b9e50f1fda1c9",
+    "url": "/static/js/main.c184cb71.chunk.js"
+  },
+  {
+    "revision": "afb178ddda4a40468285",
+    "url": "/static/js/runtime-main.9503cc2f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
